f5d8b6ab073594b8cfd77cc930ca6eed56f23d7c
Joyo <75166100+JoyoRichard007@users.noreply.github.com>
2023-09-15 11:30:51 +0300

    Edit

--- Front-End/src/app/edit/edit.component.ts
+++ Front-End/src/app/edit/edit.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.scss']
 })
+  
 export class EditComponent implements OnInit {
 
   constructor() { }
